1e6b31c3e2f7061686e1dc84a75d15839b114e28
edgracilla <ed.gracilla@gmail.com>
2016-12-16 15:23:19 +0800

    code cleanup

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var platform 			= require('./platform'),
-	isArray 				= require('lodash.isarray'),
+var platform 		= require('./platform'),
+	isArray 		= require('lodash.isarray'),
 	isPlainObject 	= require('lodash.isplainobject'),
 	requestPromise 	= require('request-promise'),
-	async						= require('async'),
+	async 			= require('async'),
 	_config;
 
 // used in mocha test notif
@@ -79,11 +79,6 @@ let _processRequest = (action, device, callback) => {
 		});
 };
 
-/**
- * Emitted when the platform issues a sync request. Means that the device sync plugin
- * should fetch updates from the 3rd party service.
- * @param {string} lastSyncDate ISO Timestamp from when the last sync happened. Allows you to fetch data from a certain point in time.
- */
 platform.on('sync', function (lastSyncDate) {
 	
 	_processRequest('sync', {}, (devices) => {
@@ -92,7 +87,6 @@ platform.on('sync', function (lastSyncDate) {
 			return platform.handleException(new Error(`Invalid data received. Data must be a valid Array/JSON Object or a collection of objects. Data: ${devices}`));
 
 		async.each(devices, (device, done) => {
-			
 			let param = {
 				_id: device.id,
 				name: device.name,
@@ -101,69 +95,32 @@ platform.on('sync', function (lastSyncDate) {
 
 			platform.syncDevice(JSON.stringify(param), done);
 
-		}, () => { _notifyDone('GET'); });
+		}, () => {
+			_notifyDone('GET'); 
+		});
 	});
 
 });
 
-/**
- * Emitted when a device is registered in the Reekoh platform.
- * @param {object} device The Device details and/or information.
- */
 platform.on('adddevice', function (device) {
-
 	_processRequest('add', device);
-
 });
 
-/**
- * Emitted when a device is update in the Reekoh platform.
- * @param {object} device The Device details and/or information.
- */
 platform.on('updatedevice', function (device) {
-
 	_processRequest('update', device);
-
 });
 
-/**
- * Emitted when a device is deleted in the Reekoh platform.
- * @param {object} device The Device details and/or information.
- */
 platform.on('removedevice', function (device) {
-
 	_processRequest('remove', device);
-
 });
 
-/**
- * Emitted when the platform shuts down the plugin. The Device Sync should perform cleanup of the resources on this event.
- */
 platform.once('close', function () {
-	let d = require('domain').create();
-
-	d.once('error', function (error) {
-		console.error(error);
-		platform.handleException(error);
-		platform.notifyClose();
-		d.exit();
-	});
-
-	d.run(function () {
-		// TODO: Release all resources and close connections etc.
-		platform.notifyClose(); // Notify the platform that resources have been released.
-		d.exit();
-	});
+	platform.notifyClose();
 });
 
-/**
- * Emitted when the platform bootstraps the plugin. The plugin should listen once and execute its init process.
- * Afterwards, platform.notifyReady() should be called to notify the platform that the init process is done.
- * @param {object} options The parameters or options. Specified through config.json.
- */
 platform.once('ready', function (options) {
 	_config = options;
 
 	platform.notifyReady();
-	platform.log('Device sync has been initialized.');
+	platform.log('HCP-RDMS Device sync has been initialized.');
 });
